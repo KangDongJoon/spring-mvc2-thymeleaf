92e8f14844e733b09d8b22aa7a9582eb73e6c00a
KangDongJoon <ehdwns1119@naver.com>
2025-04-28 18:12:09 +0900

    Thymeleaf - 리터럴

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -79,6 +79,13 @@ public class BasicController {
         model.addAttribute("param2", "data2");
         return "basic/link";
     }
+
+    @GetMapping("/literal")
+    public String literal(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/literal";
+    }
+
     @Data
     static class User {
         private String username;
@@ -88,7 +95,6 @@ public class BasicController {
             this.username = username;
             this.age = age;
         }
-
     }
 
 }
